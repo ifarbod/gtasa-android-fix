55354ceca8a538e79c153cc4be3da18627ee3191
iFarbod <ifarbod@outlook.com>
2016-09-08 14:33:51 +0430

    Initial Launcher and slightly modified premake scipt

---
+++ Client/Launcher/Main.cpp
@@ -0,0 +1,14 @@
+// Client Launcher entry point
+// Author(s):       iFarbod <ifarbod@outlook.com>
+//
+// Copyright (c) 2015-2016 San Andreas Online
+//
+// Distributed under the MIT license (See accompanying file LICENSE or copy at
+// https://opensource.org/licenses/MIT)
+
+#include <windows.h>
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR strCmdLine, int nCmdShow)
+{
+    return 0;
+}
--- premake5.lua
+++ premake5.lua
@@ -1,1 +1,12 @@
--- Premake5 script
+-- SAO's Premake5 script
+-- Author(s):       iFarbod <ifarbod@outlook.com>
+--
+-- Copyright (c) 2015-2016 San Andreas Online
+--
+-- Distributed under the MIT license (See accompanying file LICENSE or copy at
+-- https://opensource.org/licenses/MIT)
+
+
+workspace "SAO"
+	location "Build"
+-- TODO
