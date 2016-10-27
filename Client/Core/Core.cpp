ifarbod/gtasa-android-fix

commit cc1ddc95379d70f5d45d38a6d906d5ac15a3f806

Tweak the project to Win32 only

Client/Core/Core.cpp
@@ -1,0 +1,17 @@
+// Core module
+// Author(s):       iFarbod <ifarbod@outlook.com>
+//
+// Copyright (c) 2015-2016 San Andreas Online
+//
+// Distributed under the MIT license (See accompanying file LICENSE or copy at
+// https://opensource.org/licenses/MIT)
+
+#include "Core.hpp"
+
+Core::Core()
+{
+}
+
+Core::~Core()
+{
+}

Client/Core/Core.hpp
@@ -1,0 +1,19 @@
+// Core module
+// Author(s):       iFarbod <ifarbod@outlook.com>
+//
+// Copyright (c) 2015-2016 San Andreas Online
+//
+// Distributed under the MIT license (See accompanying file LICENSE or copy at
+// https://opensource.org/licenses/MIT)
+
+#pragma once
+
+class Core
+{
+public:
+    Core();
+    ~Core();
+
+private:
+
+};

Client/Core/Main.cpp
@@ -7,7 +7,10 @@
 // Distributed under the MIT license (See accompanying file LICENSE.md or copy at
 // https://opensource.org/licenses/MIT)
 
-#include <Win32\Util.Win32Headers.hpp>
+#include "pch.hpp"
+#include "Core.hpp"
+
+Core * g_pCore;
 
 BOOL WINAPI DllMain(HINSTANCE hDllInstance, DWORD dwReason, LPVOID lpvReserved)
 {
@@ -17,7 +20,7 @@ BOOL WINAPI DllMain(HINSTANCE hDllInstance, DWORD dwReason, LPVOID lpvReserved)
 
         //SetCurrentDirectory(gtaDir)
         //SetDllDirectory(SAOPath("SAO"));
-        // initialize
+        g_pCore = new Core();
         //SetCurrentDirectory(gtaDir)
     }
 
@@ -25,7 +28,8 @@ BOOL WINAPI DllMain(HINSTANCE hDllInstance, DWORD dwReason, LPVOID lpvReserved)
     {
         TerminateProcess(GetCurrentProcess(), 0);
 
-        // shut down
+        delete g_pCore;
+        g_pCore = nullptr;
     }
     return TRUE;
 }

Client/Core/pch.cpp
@@ -1,1 +1,9 @@
+// Precompiled header generator
+// Author(s):       iFarbod <ifarbod@outlook.com>
+//
+// Copyright (c) 2015-2016 San Andreas Online
+//
+// Distributed under the MIT license (See accompanying file LICENSE or copy at
+// https://opensource.org/licenses/MIT)
+
 #include "pch.hpp"

Client/Core/pch.hpp
@@ -1,0 +1,11 @@
+// Precompiled header generator
+// Author(s):       iFarbod <ifarbod@outlook.com>
+//
+// Copyright (c) 2015-2016 San Andreas Online
+//
+// Distributed under the MIT license (See accompanying file LICENSE or copy at
+// https://opensource.org/licenses/MIT)
+
+#pragma once
+
+#include <Windows.h>

Client/Core/premake5.lua
@@ -4,8 +4,10 @@ project "Core"
     targetname "Core"
     targetdir(buildpath("SAO"))
     
-    --pchheader "pch.hpp"
-    --pchsource "pch.cpp"
+    pchheader "pch.hpp"
+    pchsource "pch.cpp"
+    
+    links { "RakNet", "Lua" }
     
     vpaths { 
         ["Headers/*"] = { "**.hpp", "**.h" },

Client/Launcher/Kernel32.cpp
@@ -1,10 +1,0 @@
-// Forwarding kernel32 functions to avoid antiviruses
-// Author(s):       iFarbod <ifarbod@outlook.com>
-//
-// Copyright (c) 2015-2016 San Andreas Online
-//
-// Distributed under the MIT license (See accompanying file LICENSE or copy at
-// https://opensource.org/licenses/MIT)
-
-#include "Kernel32.hpp"
-

Client/Launcher/Kernel32.hpp
@@ -1,28 +1,0 @@
-// Forwarding kernel32 functions to avoid antiviruses
-// Author(s):       iFarbod <ifarbod@outlook.com>
-//
-// Copyright (c) 2015-2016 San Andreas Online
-//
-// Distributed under the MIT license (See accompanying file LICENSE or copy at
-// https://opensource.org/licenses/MIT)
-
-#pragma once
-
-#include <Util.Win32Headers.hpp>
-
-using VirtualAllocEx_t = LPVOID(WINAPI *)(
-    HANDLE hProcess,
-    LPVOID lpAddress,
-    SIZE_T dwSize,
-    DWORD flAllocationType,
-    DWORD flProtect
-    );
-
-class Kernel32
-{
-public:
-
-
-private:
-    VirtualAllocEx_t m_pfnVirtualAllocEx;
-};

commit 2777270a2d2618c25240633ff57e984eede9e9c5

Fix build error introduced in cc1ddc95

Client/Core/Core.cpp
@@ -6,6 +6,7 @@
 // Distributed under the MIT license (See accompanying file LICENSE or copy at
 // https://opensource.org/licenses/MIT)
 
+#include "pch.hpp"
 #include "Core.hpp"
 
 Core::Core()