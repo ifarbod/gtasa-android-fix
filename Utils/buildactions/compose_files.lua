ef7263478ece4c954017fcabe376ed69cccb50a9
iFarbod <ifarbod@outlook.com>
2016-11-12 01:08:08 +0330

    Fix CEF binary output directory

--- Utils/buildactions/compose_files.lua
+++ Utils/buildactions/compose_files.lua
@@ -5,6 +5,7 @@ premake.modules.compose_files = {}
 -- Config variables
 local BIN_DIR = "Bin"
 local DATA_DIR = "Shared/Data"
+local CEF_DIR = "Vendor/cef3"
 local OUTPUT_DIR = "InstallFiles"
 
 newaction {
@@ -15,11 +16,18 @@ newaction {
         os.mkdir(OUTPUT_DIR)
         
         -- Copy data files
-        os.copydir(DATA_DIR.."/SAO", OUTPUT_DIR.."/SAO")
+        os.copydir(DATA_DIR .. "/SAO", OUTPUT_DIR .. "/SAO")
         
         -- Copy the binaries
         os.copydir(BIN_DIR, OUTPUT_DIR, "**.exe")
         os.copydir(BIN_DIR, OUTPUT_DIR, "**.dll")
+        
+        -- Copy cef3 data files
+        os.copydir(CEF_DIR .. "/Release", OUTPUT_DIR .. "/SAO", "*.dll")
+        os.copydir(CEF_DIR .. "/Release", OUTPUT_DIR .. "/SAO", "*.bin")
+        os.copydir(CEF_DIR .. "/Resources", OUTPUT_DIR .. "/SAO", "icudtl.dat")
+        os.copydir(CEF_DIR .. "/Resources", OUTPUT_DIR .. "/SAO/cef", "*.pak")
+        os.copydir(CEF_DIR .. "/Resources/locales", OUTPUT_DIR .. "/SAO/cef/locales", "en-US.pak")
     end
 }
 
--- Utils/buildactions/install_data.lua
+++ Utils/buildactions/install_data.lua
@@ -5,6 +5,7 @@ premake.modules.install_data = {}
 -- Config variables
 local BIN_DIR = "Bin"
 local DATA_DIR = "Shared/Data"
+local CEF_DIR = "Vendor/cef3"
 
 newaction {
     trigger = "install_data",
@@ -13,9 +14,17 @@ newaction {
     execute = function()
         -- Make Bin directory if it doesn't exists
         os.mkdir(BIN_DIR)
+        os.mkdir(BIN_DIR .. "/SAO")
 
         -- Copy data files
         os.copydir(DATA_DIR, BIN_DIR)
+        
+        -- Copy cef3 data files
+        os.copydir(CEF_DIR .. "/Release", BIN_DIR .. "/SAO", "*.dll")
+        os.copydir(CEF_DIR .. "/Release", BIN_DIR .. "/SAO", "*.bin")
+        os.copydir(CEF_DIR .. "/Resources", BIN_DIR .. "/SAO", "icudtl.dat")
+        os.copydir(CEF_DIR .. "/Resources", BIN_DIR .. "/SAO/cef", "*.pak")
+        os.copydir(CEF_DIR .. "/Resources/locales", BIN_DIR .. "/SAO/cef/locales", "en-US.pak")
     end
 }
 
