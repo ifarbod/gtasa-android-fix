383bc4b8e1aed5dfd363da51fa24bcdb39f37258
iFarbod <ifarbod@outlook.com>
2016-11-15 01:00:07 +0330

    Add error message on dll injection fail

--- Client/Launcher/Main.cpp
+++ Client/Launcher/Main.cpp
@@ -60,6 +60,7 @@ int APIENTRY wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPWSTR strCm
     if (!RemoteLoadLibrary(pi.hProcess, PathJoin(GetSAOPath("SAO"), CLIENT_CORE_NAME DEBUG_SUFFIX LIB_EXTENSION)))
     {
         TerminateProcess(pi.hProcess, 0);
+        MessageBoxW(nullptr, L"Failed to inject the DLL", nullptr, MB_ICONSTOP);
         return 1;
     }
 
