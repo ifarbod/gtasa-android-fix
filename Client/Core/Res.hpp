1c02fad1674928681c009114dc7a01415e2c39a1
AliAssassiN <ailsynthax@gmail.com>
2016-11-15 21:41:29 +0330

    Improved version info
    Now version info in resource files can be placed with a simple function.

---
+++ Client/Core/Res.hpp
@@ -0,0 +1,8 @@
+// SAO Version
+// Author(s):       AliAssassiN <ailsynthax@gmail.com>
+//
+// Copyright (c) 2015-2016 The San Andreas Online Open Source Project
+//
+// Distributed under the MIT license (See accompanying file LICENSE or copy at
+// https://opensource.org/licenses/MIT)
+
