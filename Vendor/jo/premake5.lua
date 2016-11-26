c58c28086fe38b41ffd345dda2cbcd788f3831cc
iFarbod <ifarbod@outlook.com>
2016-11-26 19:48:20 +0330

    :fire: :heavy_plus_sign: Replaced libjpeg with JO library

---
+++ Vendor/jo/premake5.lua
@@ -0,0 +1,15 @@
+project "jo"
+    language "C++"
+    kind "StaticLib"
+    
+    vpaths { 
+        ["Headers/*"] = "**.hpp",
+        ["Sources/*"] = "**.cpp",
+        ["*"] = "premake5.lua"
+    }
+    
+    files {
+        "premake5.lua",
+        "*.hpp",
+        "*.cpp"
+    }
