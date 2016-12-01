410a3fc1360507aa5df98565159322e9df05d738
iFarbod <ifarbod@outlook.com>
2016-12-02 03:17:45 +0330

    Move frame related hooks to hooktimer.cpp

--- Client/Core/Hooks/HookTimer.cpp
+++ Client/Core/Hooks/HookTimer.cpp
@@ -1,4 +1,4 @@
-// Input processing hooks
+// Timer system fixes
 // Author(s):       iFarbod <ifarbod@outlook.com>
 //                  LINK/2012 <dma2012@gmail.com>
 //
@@ -15,14 +15,13 @@ using namespace Util;
 
 static HookFunction hookFunction([]()
 {
-    // Disable re-initialization of DirectInput mouse device by the game
-    MakeJMP(0x576CCC);
-    MakeJMP(0x576EBA);
-    MakeJMP(0x576F8A);
+    // No 14ms wait (which prevents the frame limiter to work properly)
+    MemPatch<u16>(0x53E923, 0x43EB);
+    MemPatch<u8>(0x53E99F, 0x10);
+    MakeNOP(0x53E9A5);
 
-    // Make sure DirectInput mouse device is set non-exclusive
-    MemPatch<u32>(0x7469A0, 0x9090C030);
-
-    // No DirectInput gamepad
-    MakeNOP(0x748813, 5);
+    // Fixed stuck strafing with 2-handed weapons with FPS > 45
+    static const float strafeCheck = 0.1f;
+    MemPatch(0x61E0C2, &strafeCheck);
+    MakeNOP(0x61E0CA, 6);
 });
--- Client/Core/Hooks/Hooks.cpp
+++ Client/Core/Hooks/Hooks.cpp
@@ -35,20 +35,10 @@ static Util::HookFunction hookFunction([]()
     // Better than SetWindowText
     MemPatch(0x619608, "San Andreas Online");
 
-    // Fixed stuck strafing with 2-handed weapons with FPS > 45
-    static const float strafeCheck = 0.1f;
-    MemPatch(0x61E0C2, &strafeCheck);
-    MakeNOP(0x61E0CA, 6);
-
     // Unlocked widescreen resolutions
     MemPatch<u32>(0x745B81, 0x9090587D);
     MemPatch<u32>(0x74596C, 0x9090127D);
     MakeNOP(0x745970, 2);
     MakeNOP(0x745B85, 2);
     MakeNOP(0x7459E1, 2);
-
-    // No 14ms wait
-    MemPatch<u16>(0x53E923, 0x43EB);
-    MemPatch<u8>(0x53E99F, 0x10);
-    MakeNOP(0x53E9A5);
 });
