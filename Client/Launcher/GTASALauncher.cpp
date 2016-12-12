85b990bf88d7c9514969ad65425b39ca65afdb3e
iFarbod <ifarbod@users.noreply.github.com>
2016-12-12 23:20:35 +0330

    Fix DLL loading

--- Client/Launcher/GTASALauncher.cpp
+++ Client/Launcher/GTASALauncher.cpp
@@ -94,7 +94,7 @@ void GTASALauncher::Launch(const char* gamePath)
     // Change CdStream semaphore to allow more than 2 SA instances
     //Util::StrCpy_(0x858AD4, "semaphore");
 
-    LoadLibraryA(CLIENT_CORE_NAME LIB_EXTENSION DEBUG_SUFFIX);
+    LoadLibraryA(CLIENT_CORE_NAME DEBUG_SUFFIX LIB_EXTENSION);
 
     // get the entry point
     void(*entryPoint)();
