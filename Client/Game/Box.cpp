8cf9e36880eaba6c54e893c65dec9ca3a0dbe218
iFarbod <ifarbod@outlook.com>
2017-03-05 17:53:49 +0330

    Add reversed CBox class

---
+++ Client/Game/Box.cpp
@@ -0,0 +1,36 @@
+// Box
+// Author(s):       iFarbod <>
+//
+// Copyright (c) 2015-2017 Project CtNorth
+//
+// Distributed under the MIT license (See accompanying file LICENSE or copy at
+// https://opensource.org/licenses/MIT)
+
+#include "Box.hpp"
+
+namespace ctn::SA
+{
+
+void CBox::Set(const CVector& sup, const CVector& inf)
+{
+    m_supremum = sup;
+    m_infimum = inf;
+}
+
+void CBox::Recalc()
+{
+    if (m_supremum.m_x > m_infimum.m_x)
+    {
+        Swap<f32>(m_infimum.m_x, m_supremum.m_x);
+    }
+    if (m_supremum.m_y > m_infimum.m_y)
+    {
+        Swap<f32>(m_infimum.m_y, m_supremum.m_y);
+    }
+    if (m_supremum.m_z > m_infimum.m_z)
+    {
+        Swap<f32>(m_infimum.m_z, m_supremum.m_z);
+    }
+}
+
+}
---
+++ Client/Game/Box.hpp
@@ -0,0 +1,40 @@
+// Box
+// Author(s):       iFarbod <>
+//
+// Copyright (c) 2015-2017 Project CtNorth
+//
+// Distributed under the MIT license (See accompanying file LICENSE or copy at
+// https://opensource.org/licenses/MIT)
+
+#pragma once
+
+#include "Common.hpp"
+#include "Vector.hpp"
+
+namespace ctn::SA
+{
+
+class CBox
+{
+public:
+    // Default constructor
+    CBox() :
+        m_supremum(CVector::ONE),
+        m_infimum(-CVector::ONE)
+    {}
+
+    // Initialize this box using two points.
+    void Set(const CVector& sup, const CVector& inf);
+    // Recalculates box corners - if left > right then Swap(left, right).
+    void Recalc();
+
+private:
+    // Supremum vertex (contains largest values)
+    CVector m_supremum; // +0x0
+    // Infimum vertex (contains smallest values)
+    CVector m_infimum;  // +0xC
+};
+
+VALIDATE_SIZE(CBox, 0x18);
+
+}
