4a1218e370c60f354bea7a4ce5b0d9fb02965f45
iFarbod <ifarbod@outlook.com>
2017-03-07 03:14:49 +0330

    Add basic CFont and its dependencies

---
+++ Client/Game/Font.cpp
@@ -0,0 +1,23 @@
+// Font.cpp
+//
+// Author(s):       iFarbod <>
+//
+// Copyright (c) 2015-2017 Project CtNorth
+//
+// Distributed under the MIT license (See accompanying file LICENSE or copy at
+// https://opensource.org/licenses/MIT)
+
+#include "Font.hpp"
+
+namespace ctn::SA
+{
+
+void CFont::Initialize()
+{
+}
+
+void CFont::Shutdown()
+{
+}
+
+}
---
+++ Client/Game/Font.hpp
@@ -0,0 +1,31 @@
+// Im2D Font renderer
+//
+// Author(s):       iFarbod <>
+//
+// Copyright (c) 2015-2017 Project CtNorth
+//
+// Distributed under the MIT license (See accompanying file LICENSE or copy at
+// https://opensource.org/licenses/MIT)
+
+#pragma once
+
+#include "Common.hpp"
+#include "FontDetails.hpp"
+#include "FontRenderState.hpp"
+#include "Sprite2d.hpp"
+
+namespace ctn::SA
+{
+
+class CFont
+{
+public:
+    // Initialize Font API
+    static void Initialize();
+    // Shutdown Font API
+    static void Shutdown();
+private:
+
+};
+
+}
