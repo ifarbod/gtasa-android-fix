ddce2959690c79b7407f8888d5dc8c1e6397a820
iFarbod <ifarbod@outlook.com>
2017-03-07 03:17:51 +0330

    :art: Mark CPlayerPed offsets and describe the unknown field

--- Client/Game/PlayerPed.hpp
+++ Client/Game/PlayerPed.hpp
@@ -17,8 +17,8 @@ namespace ctn::SA
 class CPlayerPed : public CPed
 {
 public:
-    CPed* m_targettedPed;
-    s32 m_field7A0;
+    CPed* m_targettedPed; // +0x79C
+    s32 m_field7A0;       // +0x7A0 TimeInMS related to 3rd person aiming
 };
 
 //VALIDATE_SIZE(CPlayerPed, 0x7A4);
