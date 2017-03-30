7fd8adbf1609d4c7ee5e41aad9235164e0f9d15d
iFarbod <ifarbod@outlook.com>
2017-03-30 17:31:50 +0430

    :new: Add LZ4 v1.7.5

---
+++ Vendor/lz4/premake5.lua
@@ -0,0 +1,16 @@
+project "LZ4"
+    language "C++"
+    kind "StaticLib"
+
+    vpaths {
+        ["Headers/*"] = "**.h",
+        ["Sources/*"] = "**.c",
+        ["*"] = "premake5.lua"
+    }
+
+    files {
+        "premake5.lua",
+        "**.h",
+        "**.c"
+    }
+
