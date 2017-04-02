e078290d5c5a0b1e4d35fc3c0550511609ab0d9e
dkluin <dcluynserf@gmail.com>
2017-04-02 10:58:39 +0200

    added new CPedModelInfo

--- Client/Game/PedModelInfo.hpp
+++ Client/Game/PedModelInfo.hpp
@@ -17,31 +17,42 @@ namespace ctn::SA
 class CPedModelInfo : public CClumpModelInfo
 {
 public:
-    s32 m_motionAnimGroup; // AssocGroupID
-    s32 m_defPedType;
-    s32 m_defPedStats; // ePedStats
-
-    union
-    {
-        struct
-        {
-            u32 m_drivingMask;
-        };
-        struct
-        {
-            u16 m_canDriveCars;
-            u16 m_pedFlags;
-        };
-    };
-    class CColModel* m_hitColModel;
-    u8 m_firstRadioStation;
-    u8 m_secondRadioStation;
-    u8 m_race;
-    u8 m_field3B;
-    s16 m_audioPedType;
-    s16 m_firstVoice;
-    s16 m_lastVoice;
-    s16 m_nextVoice;
+	uint32_t m_dwAnimType;
+	uint32_t m_dwPedType;
+	uint32_t m_dwStatType;
+	struct
+	{
+		uint16_t m_wNormalCar : 1;
+		uint16_t m_wPoorFamilyCar : 1;
+		uint16_t m_wRichFamilyCar : 1;
+		uint16_t m_wExecutiveCar : 1;
+		uint16_t m_wWorkerCar : 1;
+		uint16_t m_wBigCar : 1;
+		uint16_t m_wTaxiCar : 1;
+		uint16_t m_wMoped : 1;
+		uint16_t m_wMotorBike : 1;
+		uint16_t m_wLeisureBoat : 1;
+		uint16_t m_wWorkerBoat : 1;
+		uint16_t m_wBicycle : 1;
+		uint16_t m_wOnFoot : 1;
+	} m_wCarCanDriveMask;
+	struct
+	{
+		bool m_bBuysDrugs : 1;
+		bool m_bUnk1 : 1;
+		bool m_bUnk2 : 1;
+		bool m_bUnk3 : 1;
+		bool m_bUnk4 : 1;
+		bool m_bUnk5 : 1;
+		bool m_bUnk6 : 1;
+		bool m_bUnk7 : 1;
+	} m_wPedFlags;
+	uint32_t field_34;
+	uint8_t m_nRadio1;
+	uint8_t m_nRadio2;
+	uint16_t m_wModelIndex;
+	uint16_t m_wVoiceType;
+	uint16_t m_wVoice1, m_wVoice2, m_wVoice3;
 };
 
 VALIDATE_SIZE(CPedModelInfo, 0x44);
