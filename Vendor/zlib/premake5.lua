aa7e58c8fbcbbf48fec42bbdab720c95ffffd104
iFarbod <ifarbod@outlook.com>
2017-04-04 17:03:12 +0430

    Move minizip to zlib

--- Vendor/zlib/premake5.lua
+++ Vendor/zlib/premake5.lua
@@ -3,14 +3,49 @@ project "zlib"
     kind "StaticLib"
     targetname "zlib"
 
-    vpaths {
+    vpaths
+    {
         ["Headers/*"] = "**.h",
         ["Sources/*"] = "**.c",
         ["*"] = "premake5.lua"
     }
 
-    files {
+    files
+    {
         "premake5.lua",
         "*.h",
         "*.c"
     }
+
+project "Zip"
+    language "C"
+    kind "StaticLib"
+    targetname "zip"
+
+    links "zlib"
+    
+    includedirs
+    {
+        "../zlib"
+    }
+
+    vpaths
+    {
+        ["Headers/*"] = "**.h",
+        ["Sources"] = "*.c",
+        ["*"] = "premake5.lua"
+    }
+
+    files
+    {
+        "premake5.lua",
+        "minizip/*.h",
+        "minizip/*.c"
+    }
+
+    filter "system:linux"
+        excludes
+        {
+            "minizip/iowin32.h",
+            "minizip/iowin32.c"
+        }
