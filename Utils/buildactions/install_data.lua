8a7ff5cda7b67ce7ce2d4ae7129bf53b051848ca
iFarbod <ifarbod@outlook.com>
2017-04-06 18:12:37 +0430

    Change 'Shared/Data' to 'Data' in install_data build action

--- Utils/buildactions/install_data.lua
+++ Utils/buildactions/install_data.lua
@@ -12,7 +12,7 @@ premake.modules.install_data = {}
 
 -- Config variables
 local BIN_DIR = "Bin"
-local DATA_DIR = "Shared/Data"
+local DATA_DIR = "Data"
 local CEF_DIR = "Vendor/cef3"
 
 newaction {
