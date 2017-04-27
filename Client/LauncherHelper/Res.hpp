b16598e5ecd4534ae2eb08ea8a8f3611badc1206
iFarbod <ifarbod@outlook.com>
2017-04-27 17:07:36 +0430

    Remove temporary launcher helper

--- Client/LauncherHelper/Res.hpp
+++
@@ -1,9 +0,0 @@
-// res.hpp
-// Author(s):       AliAssassiN <ailsynthax@gmail.com>
-//
-// Copyright (c) 2015-2017 CTNorth Team
-//
-// Distributed under the MIT license (See accompanying file LICENSE or copy at
-// https://opensource.org/licenses/MIT)
-
-#define IDR_GAMEBIN 101
